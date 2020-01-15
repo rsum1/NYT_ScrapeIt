91ab7354b2ff4661cd204bf900341180b34e32ac
rsum1 <ryanjsumpter+github@gmail.com>
2020-01-15 13:30:35 -0800

    mongo connected, article &  comment models done

--- server.js
+++ server.js
@@ -12,18 +12,28 @@ app.use(
   })
 )
 
+//connect app to public folder
+app.use(express.static(process.cwd() + "/public"))
+
 //Require handlebars
-var exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars")
 app.engine(
   "handlebars",
   exphbs({
     defaultLayout: "main"
   })
-);
-app.set("view engine", "handlebars");
+)
+app.set("view engine", "handlebars")
+
+mongoose.connect("mongodb://localhost/scraper")
+var db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", function () {
+  console.log("Connected to Mongoose!")
+})
 
 //Create localhost port on port 3000
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log("Listening on PORT " + port);
-});
+  console.log("Listening on PORT " + port)
+})
